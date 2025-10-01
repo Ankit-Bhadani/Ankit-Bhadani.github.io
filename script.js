67f13815952ba366763488ff516fd78fb1865e4c
Ankit-Bhadani <ankitbha00@gmail.com>
2025-10-01 17:21:25 +0530

    Added some new JS for todo list and Toggle button for Hobbies

--- script.js
+++ script.js
@@ -3,3 +3,48 @@ const btn = document.getElementById("changeColor");
 btn.addEventListener("click", () => {
   document.body.style.backgroundColor = "#e0f7fa"; // light cyan
 });
+let userName = "Ankit";
+let age = 20;
+let isStudent = true;
+let hobbies = ["Reading", "Coding", "Sports"];
+let person = { name: "Ankit", age: 20, city: "Delhi" };
+
+console.log(userName);
+console.log(age);
+console.log(isStudent);
+console.log(hobbies);
+console.log(person);
+
+const addbtn = document.getElementById("addtodo")
+const addinput = document.getElementById("todoinput")
+const todolist = document.getElementById("todoList")
+
+let todos = [];
+
+addbtn.addEventListener("click",() => {
+  const task = addinput.value;
+  if(task!==""){
+    todos.push(task);
+    todolist.innerHTML="";
+    for(let t of todos){
+      const li = document.createElement("li");
+      li.textContent = t;
+      todolist.appendChild(li);
+    }
+    addinput.value="";
+  } else {
+    alert("Please enter a task!");
+  }
+});
+
+const toggle = document.getElementById("toggle")
+const selection = document.getElementById("hobbies")
+
+toggle.addEventListener("click",()=>{
+  if(selection.style.display==="none"){
+    selection.style.display="block";
+  }
+  else{
+    selection.style.display="none";
+  }
+})
